006151e22de6d59c6b6b31056fb5afb283b04425
Code By Tausif <tausifurrehman447@gmail.com>
2025-12-12 15:00:51 +0500

    Tausif Create Opreator file

---
+++ operator_in_cpp.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // Operator in C++
+    int a=4;
+    a+=4;
+    cout<<a<<endl;
+    a-=4;
+    cout<<a<<endl;
+    a*=4;
+    cout<<a<<endl;
+    a/=4;
+    cout<<a;
+}
